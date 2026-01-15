f9512e692a594394a73df75b5b010bbcd9552b68
42-zorin <abidaux@student.42lehavre.fr>
2026-01-15 03:19:46 +0100

    animal is now virtual & good main

--- ex02/include/Animal.hpp
+++ ex02/include/Animal.hpp
@@ -6,7 +6,7 @@
 /*   By: adrien <adrien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 11:55:26 by abidaux           #+#    #+#             */
-/*   Updated: 2026/01/15 00:41:19 by adrien           ###   ########.fr       */
+/*   Updated: 2026/01/15 03:14:00 by adrien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ class Animal {
         Animal(const Animal& other);
         Animal& operator=(const Animal& other);
         virtual ~Animal();
-        virtual void makeSound() const;
+        virtual void makeSound() const = 0;
         std::string getType() const;
     protected :
         std::string type;
