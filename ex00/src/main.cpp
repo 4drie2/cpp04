da8e9b306c2467af5bf4a19a943b0c279bb9312c
42-zorin <abidaux@student.42lehavre.fr>
2026-01-15 01:48:22 +0100

    add better main test

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,36 +6,46 @@
 /*   By: adrien <adrien@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/06 12:11:28 by abidaux           #+#    #+#             */
-/*   Updated: 2026/01/15 00:41:19 by adrien           ###   ########.fr       */
+/*   Updated: 2026/01/15 01:46:24 by adrien           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/Animal.hpp"
-#include "../include/Cat.hpp"
 #include "../include/Dog.hpp"
+#include "../include/Cat.hpp"
+#include "../include/WrongAnimal.hpp"
 
-int main(void){
-    std::cout << "my test main" << std::endl;
-    {
-        Animal genericAnimal;
-        genericAnimal.makeSound();
+int main()
+{
+    std::cout << "=== SUJECT TESTS (Correct) ===" << std::endl;
+    const Animal* meta = new Animal();
+    const Animal* j = new Dog();
+    const Animal* i = new Cat();
 
-        Cat cat;
-        cat.makeSound();
+    std::cout << j->getType() << " " << std::endl;
+    std::cout << i->getType() << " " << std::endl;
 
-        Dog dog;
-        dog.makeSound();
-    }
-    std::cout << "subject test main" << std::endl;
-    {
-        const Animal* meta = new Animal();
-        const Animal* j = new Dog();
-        const Animal* i = new Cat();
-        std::cout << j->getType() << " " << std::endl;
-        std::cout << i->getType() << " " << std::endl;
-        i->makeSound(); //will output the cat sound!
-        j->makeSound();
-        meta->makeSound();
-        return 0;
-    }
+    i->makeSound(); // Doit afficher le son du chat (Meow)
+    j->makeSound(); // Doit afficher le son du chien (Woof)
+    meta->makeSound();
+
+    delete meta;
+    delete j;
+    delete i;
+
+    std::cout << std::endl;
+
+    std::cout << "=== WRONG ANIMAL TESTS (No Virtual) ===" << std::endl;
+    const WrongAnimal* wrongMeta = new WrongAnimal();
+    const WrongAnimal* wrongCat = new WrongCat();
+
+    std::cout << wrongCat->getType() << " " << std::endl;
+
+    wrongCat->makeSound();
+    wrongMeta->makeSound();
+
+    delete wrongMeta;
+    delete wrongCat;
+
+    return 0;
 }
